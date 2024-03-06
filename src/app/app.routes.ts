9dff4320abf7dbc7523c97f7aba1d8952165ba73
WhiteSoule <poldrugo.alex@gmail.com>
2024-03-06 19:29:14 +0100

    made homePage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,19 @@ import { Routes } from '@angular/router';
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {AppComponent} from "./app.component";
 import { SignInComponent } from './sign-in/sign-in.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
   {
-    path:'#',
-    component:AppComponent
+    path:'',
+    component: HomePageComponent
   },
   {
     path:'register',
-    component:SignUpComponent
+    component: SignUpComponent
   },
   {
     path:'login',
-    component:SignInComponent
+    component: SignInComponent
   }
 ];
