0e6942b940d1f92cc91d970c8476a10426fc8fbc
WhiteSoule <poldrugo.alex@gmail.com>
2024-03-13 01:46:40 +0100

    made article page responsive

--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ProfileService } from '../../services/profile.service';
 import { Article } from '../../interfaces/article';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { AuthenticationService } from '../../services/authentication.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-article-page',
@@ -12,7 +13,8 @@ import { AuthenticationService } from '../../services/authentication.service';
   imports: [
     CommonModule,
     NgFor,
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.css'
@@ -21,12 +23,14 @@ export class ArticlePageComponent {
 
   articleSlug:string
   article:Article|undefined
+  currentUser:string|undefined
   constructor(
     private articleService:ArticleService,
     private router:Router,
     private route:ActivatedRoute,
     private profileService:ProfileService,
-    private authService:AuthenticationService
+    private authService:AuthenticationService,
+    private toaster:ToastrService
   ){
     this.articleSlug = this.route.snapshot.params['articleSlug']
   }
@@ -38,14 +42,30 @@ export class ArticlePageComponent {
           this.article = value.article
         }
       })
+    this.authService.getUser()
+      .subscribe({
+        next: value => this.currentUser = value.user.username
+      })
   }
 
   followUser(){
     this.profileService.followUser(this.article?.author.username!)
+    .subscribe({
+      next: value => {
+        this.article!.author = value.profile
+        this.toaster.success('you are following '+value.profile.username)
+      }
+    })
   }
 
   unfollowUser(){
     this.profileService.unFollowUser(this.article?.author.username!)
+      .subscribe({
+        next: value => {
+          this.article!.author = value.profile
+          this.toaster.success('you are not following '+value.profile.username+' anymore')
+        }
+      })
   }
 
   favorite(){
@@ -64,7 +84,17 @@ export class ArticlePageComponent {
     }
   }
 
+  deleteArticle(){
+    this.articleService.deleteArticle(this.article!.slug)
+      .subscribe({
+        next: value => {
+          this.toaster.success(`Article ${this.article?.slug} was deleted`)
+          this.router.navigate(['/'])
+        }
+      })
+  }
+
   get isUserAuthor():boolean{
-    return this.authService.currentUserUsername === this.article?.author.username
+    return this.currentUser === this.article?.author.username
   }
 }
